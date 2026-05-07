3d670c830855312eeb995f157323e30fb7b32415
Arinze <ihimarinze@gmail.com>
2026-05-07 11:08:34 +0100

    Fixed infinite loading on first launch

--- lib/data/repos/setting_repo_impl.dart
+++ lib/data/repos/setting_repo_impl.dart
@@ -1,4 +1,3 @@
-
 import 'package:habit_tracker/data/dto/mapper.dart';
 import 'package:habit_tracker/domain/entities/settings_entity.dart';
 import 'package:habit_tracker/domain/repository/settings_repo.dart';
@@ -7,14 +6,25 @@ import 'package:isar/isar.dart';
 import '../models/settings.dart';
 
 class SettingRepoImpl implements SettingsRepo {
-
   final Isar _isar;
   SettingRepoImpl(this._isar);
 
-  @override
-  Future<SettingsEntity> firstLaunch() async{
-    Settings? settings = await _isar.settings.where().findFirst();
-    return SettingsMapper.toEntity(settings!);
+  Future<void> _createSettings() async {
+    final settings = Settings()..initLaunchedDay = DateTime.now();
+    await _isar.writeTxn(() async {
+      await _isar.settings.put(settings);
+    });
   }
 
+  @override
+  Future<SettingsEntity> firstLaunch() async {
+    Settings? settings = await _isar.settings.where().findFirst();
+
+    if (settings == null) {
+      await _createSettings();
+      settings = await _isar.settings.where().findFirst();
+    }
+
+    return SettingsMapper.toEntity(settings!);
+  }
 }
